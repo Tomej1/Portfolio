a62dbcd9468ef644fefd7f6e2d95df9568021569
Tom Ellström Johansson <146933049+Tomej1@users.noreply.github.com>
2024-10-31 21:27:44 +0100

    Update jquery.galleryview.js

--- assets/js/jquery.galleryview.js
+++ assets/js/jquery.galleryview.js
@@ -1,10 +1,21 @@
 $(function() {
-    $("#imgGallery img").click(function() {
+    $("#imgGallery img").click(function(event) {
+        event.stopPropagation(); // Prevent click event from bubbling up to the document
+
         var $gallery = $(this).closest("#imgGallery");
 
+        // Toggle the 'grow' class on the clicked image
         $(this).toggleClass("grow");
 
+        // Toggle the 'shrink' class on other images
         $gallery.find("img").not(this).toggleClass("shrink");
-        
+
+        // If the clicked image is now grown, add a listener to the document to reset
+        if ($(this).hasClass("grow")) {
+            $(document).one("click", function() {
+                // Reset all images
+                $gallery.find("img").removeClass("grow shrink");
+            });
+        }
     });
-})(jQuery);
+});
